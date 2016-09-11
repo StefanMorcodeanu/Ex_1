7e0f50c12a4c033aa5e5d54509c0115453c7c6f5
Stefan <stefanmorcodeau@gmail.com>
2016-09-11 11:38:04 +0300

    Ex_2 11.09.2016 Java Collection

---
+++ Ex_2/src/TestProprety.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+
+/**
+ * Created by Stivie_Mr on 9/10/2016.
+ */
+public class TestProprety {
+    public static void main(String[] args) throws IOException {
+        FileReader reader = new FileReader("D:\\Java-Programming\\Ex_2\\src\\database.txt");
+
+        Properties p = new Properties();
+        p.load(reader);
+        p.setProperty("email", "sorosgroup@gmail.com");
+        p.store(new FileWriter("D:\\Java-Programming\\Ex_2\\src\\database.txt"), "Myfile");
+        System.out.println(p.getProperty("name"));
+        System.out.println(p.getProperty("surname"));
+        System.out.println(p.getProperty("age"));
+        System.out.println(p.getProperty("address"));
+        System.out.println(p.getProperty("email"));
+
+
+    }
+}
